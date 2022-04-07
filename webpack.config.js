5167e84f89ee296913b93e04073639bf0d6d29e8
XcQuid <creed.35@yandex.ru>
2022-04-07 19:11:35 +0300

    chore: install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     publicPath: ''
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
   }
 }
