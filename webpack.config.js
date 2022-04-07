12cab86f66937f57ae1e4ae8788fc6f507e299aa
XcQuid <creed.35@yandex.ru>
2022-04-07 19:15:29 +0300

    chore: install babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,15 @@ module.exports = {
     compress: true,
     port: 8080,
     open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules'
+      }
+    ]
   }
 }
+
