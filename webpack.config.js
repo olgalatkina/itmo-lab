58d389c466d2ce64b816dfc2498cab88708adfad
XcQuid <creed.35@yandex.ru>
2022-04-07 19:24:17 +0300

    chore: install clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { main: './scripts/index.js' },
@@ -27,7 +28,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 }
 
