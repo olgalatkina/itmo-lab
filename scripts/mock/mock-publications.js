15db8a7fabbcb555a13fd6c9101709a862412b5c
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-04-07 18:01:19 +0300

    temp commit

--- scripts/mock/mock-publications.js
+++ scripts/mock/mock-publications.js
@@ -4,7 +4,7 @@ const publicationsData = [
     title: 'Topic-driven Ensemble for Online Advertising Generation',
     autors: ['Egor Nevezhin', 'Nikolay Butakov', 'Maria Khodorchenko', 'Maxim Petrov', 'Denis Nasonov'],
     description: 'Online advertising is one of the most widespread ways to reach and increase a target audience for those selling products. Usually having a form of a banner, advertising engages users into visiting a corresponding webpage. Professional generation of banners requires creative',
-    link: '#',
+    link: '#temp',
     social: {
       vk: '#',
       facebook: '#',
