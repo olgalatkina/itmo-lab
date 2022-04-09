6b6a1e9d3fd042cc71d644d39033f5648574b190
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-04-09 21:57:52 +0300

    feat render publications cards

--- scripts/publications/PublicationsCard.js
+++ scripts/publications/PublicationsCard.js
@@ -2,15 +2,22 @@ export class PublicationsCard {
   constructor(data, selector) {
     this._data = data;
     this._selector = selector;
+
   }
 
   _showToolTip = () => {
-    this._tooltip = this._element.querySelector('.tooltip');
-    this._tooltip.classList.toggle('card__like_active');
+    this._tooltip.classList.toggle('tooltip_active');
+  }
+
+  _hideToolTip = () => {
+    this._tooltip.classList.remove('tooltip_active')
   }
 
   _setEventListeners() {
+    this._shareCard = this._element.querySelector('.card-meta__share');
+    this._tooltip = this._element.querySelector('.tooltip');
     this._shareCard.addEventListener('click', this._showToolTip);
+    this._element.addEventListener('mouseleave', this._hideToolTip);
   }
 
   _getElement() {
@@ -25,7 +32,7 @@ export class PublicationsCard {
     this._getElement();
 
     this._imageCard = this._element.querySelector('.publications-card__image');
-    this._imageCard.src = `url(../../images/publication/${this._data.image})`;
+    this._imageCard.src = `./images/publication/${this._data.image}`;
 
     this._nameCard = this._element.querySelector('.publications-card__name');
     this._nameCard.textContent = this._data.title;
@@ -40,10 +47,9 @@ export class PublicationsCard {
     this._linkCard = this._element.querySelector('.card-meta__link-more');
     this._linkCard.href = this._data.link;
 
-    this._shareCard = this._element.querySelector('.card-meta__share');
-
     this._setEventListeners();
 
     return this._element;
   }
+
 }
