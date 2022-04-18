c014723eab7ef6fde57556ad4d9cf63dac2a1f8e
XcQuid <creed.35@yandex.ru>
2022-04-18 20:03:36 +0300

    style: change fonts naming

--- webpack.config.js
+++ webpack.config.js
@@ -28,14 +28,14 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name].[hash][ext]'
+          filename: 'images/[name].[hash][ext][query]'
         }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'fonts/[name].[hash][ext]'
+          filename: 'fonts/[name][ext][query]'
         }
       },
       {
