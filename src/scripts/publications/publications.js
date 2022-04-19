66aa27821113dac233a83b056cf04cd4f06ac979
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-04-19 23:08:22 +0300

    feat: added slider, fixed shadow

--- src/scripts/publications/publications.js
+++ src/scripts/publications/publications.js
@@ -1,5 +1,16 @@
 import { PublicationsCard } from './PublicationsCard.js';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
 const generatePublicationsCard = (card) => new PublicationsCard(card, '#publications-template').generate();
 
 export { generatePublicationsCard };
+
+new Swiper ('.publications-container', {
+  modules: [ Navigation],
+  speed: 300,
+
+  navigation: {
+    nextEl: '.publications__slider-button_left',
+    prevEl: '.publications__slider-button_right',
+  },
+});
