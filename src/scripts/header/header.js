06afb0635f56a2f63f8f29620ff3983e9df03d3d
Mariya Kulakovskaya <kulakovskaya-mariya@yandex.ru>
2022-04-21 16:24:33 +0300

    feat: добавить комментарии к коду

--- src/scripts/header/header.js
+++ src/scripts/header/header.js
@@ -1,15 +1,27 @@
 import Link from './Link';
 import Anchor from './Anchor';
 
+//Тут проходит поиск сразу по двум классам, собирая в один nodelist.
 const links = document.querySelectorAll('.nav__link, .nav__sublink');
 const sections = document.querySelectorAll('section');
 const headerLogoArea = document.querySelector('.header__logo-area');
 
+// Функция offsetGetter принимается классом "якорь" и позволит высчитывать высоту меню в момент клика по ссылке
+// (необходимо для понимания высоты отступа).
 const offsetGetter = () => {
   return headerLogoArea.clientHeight;
 }
 
+//Переводим в "настоящий" массив, т.к. в дальнейшем понадобится метод filter.
+//После querySelectorAll создается не массив, а nodelist, к которому многие классические методы не применимы.
 const arrayOfLinks = Array.from(links);
+
+//Функция makeLinks перебирает все секции сайта и создает для них экземпляры класса "якорь".
+//Далее ищет в массиве ссылок соответствующую секции ссылку (проверка по href и id).
+//После происходит проверка, нашлась ли для секции ссылки.
+//Если проверка успешная - происходит создание экземпляров класса "ссылка" с присвоением им аргумента
+//в виде колбека из соответствующего класса "якорь" (соответствующей секции).
+//В конце на ссылку вешается слушатель.
 const makeLinks = () => {
     sections.forEach((section) => {
     const anchor = new Anchor(section, offsetGetter);
