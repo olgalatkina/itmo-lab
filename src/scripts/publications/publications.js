eb8394ba96e21550fedf4a4590b70cf1322d08fa
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-04-21 20:04:51 +0300

    feat: added publications slider

--- src/scripts/publications/publications.js
+++ src/scripts/publications/publications.js
@@ -5,12 +5,39 @@ const generatePublicationsCard = (card) => new PublicationsCard(card, '#publicat
 
 export { generatePublicationsCard };
 
-new Swiper ('.publications-container', {
+new Swiper ('.publications__container', {
   modules: [ Navigation],
-  speed: 300,
+  speed: 450,
+  watchOverflow: true,
+  grabCursor: true,
+  preventInteractionOnTransition: true,
+  rewind: true,
+  slidesPerView: 2,
+  slidesPerGroup: 1,
+  centeredSlides: false,
 
-  navigation: {
-    nextEl: '.publications__slider-button_left',
-    prevEl: '.publications__slider-button_right',
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 8,
+    },
+
+    350: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 8,
+    },
+
+    1270: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 30,
+    },
+  },
+
+   navigation: {
+    nextEl: '.publications__slider-button_right',
+    prevEl: '.publications__slider-button_left',
   },
 });
